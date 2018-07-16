6636e841738248740fb3d52262af1dd2808b4ac0
Michael Kasman <mck.kasman@gmail.com>
2018-07-16 13:50:26 -0500

    Uploaded HTTP request file and CMX execution.

---
+++ Web Server Files/cmxtest.php
@@ -0,0 +1,11 @@
+<form action="index.php" method="post">
+IP Address: <input type="text" name="ip"><br>
+<input type="submit" value="Submit">
+</form>
+
+<?php
+include("cmx_util.php");
+if($_POST) {
+    $req = new CMXRequest("config.json", $_POST["ip"]);
+    echo "<pre>" . json_encode($req->getResponse(), JSON_PRETTY_PRINT) . "</pre>";
+}
